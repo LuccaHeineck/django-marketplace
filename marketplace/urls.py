c294b0d234ef8dc6195ffd71d188de54fd0fb8de
Lucca <luccacheineck@gmail.com>
2024-06-14 16:36:42 -0300

    primeiras páginas feitas

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, contact
 
 urlpatterns = [
     path('', index, name = 'index'),
+    path('contact/', contact, name = 'contact'),
     path('admin/', admin.site.urls),
 ]
