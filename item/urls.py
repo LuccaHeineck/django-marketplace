34e3432bf115e20536da51a72a193cf42fbdd17d
Lucca <luccacheineck@gmail.com>
2024-06-24 16:29:27 -0300

    Deleção e edição adicionadas

--- item/urls.py
+++ item/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'item'
 
 urlpatterns = [
-    path('<int:pk>/', views.detail, name='detail')
+    path('', views.items, name='items'),
+    path('new/', views.new, name='new'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
 ]
